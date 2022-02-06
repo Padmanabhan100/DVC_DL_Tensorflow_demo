992457d3d0029bacc5a1a72b967578d8a2e0c88d
padmanabhan poraiyar <padmanabhanpaddy10@gmail.com>
2022-02-06 14:55:10 +0530

    stage3 done and callbacks cretaed

--- src/utils/callbacks.py
+++ src/utils/callbacks.py
@@ -8,7 +8,18 @@ from src.utils.all_utils import get_timestamp
 
 
 def create_and_save_checkpoint_callbacks(callbacks_dir,checkpoint_dir):
-    pass
+    checkpoint_file_path = os.path.join(checkpoint_dir,"ckpt_model.h5")
+    checkpoint_callbacks = tf.keras.callbacks.ModelCheckpoint(
+        filepath=checkpoint_file_path,
+        save_best_only=True)
+
+    ckpt_callbacks_filepath = os.path.join(callbacks_dir,"checkpoint_cb.cb")
+    joblib.dump(checkpoint_callbacks,ckpt_callbacks_filepath)
+
+    logging.indo(f"checkpoint callbacks are saved as binary file at {ckpt_callbacks_filepath}")
+
+
+
 def create_and_save_tensorboard_callbacks(callbacks_dir,tensorboard_log_dir):
     unique_name = get_timestamp("tb_logs")
     tb_running_log_dir = os.path.join(tensorboard_log_dir, unique_name)
